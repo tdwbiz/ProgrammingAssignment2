0d5fcffaa70ffa3108eefc5561dcc2d6b5bd27cc
tdwbiz <tdwbiz@gmail.com>
2015-03-16 22:14:48 -0400

    tdwbiz 20150316 10:14pm

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##Functions to Manupliate Matrix
+##1. makeCacheMatrix - take matrix as input, and cache it's inverse.
+##2. cacheSolve - take matric as input and return inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #init local matrix localMatrixX to NULL 
+        localMatrixX <- NULL
+        
+        #create setter function to set passed parameter matix y to cacheX, and set cacheM to NULL in cache		
+        setfunction <- function(y) {
+                cacheX <<- y
+                cacheM <<- NULL       
+        }
+        
+        #create getter function to get the matrix passed in the command line call to '$set
+        getfunction <- function() cacheX 
+        
+        #create setter function to set the value of cacheM in cache to the value of local_m localMatrixX in the call to '$set_cacheM
+        set_cacheM <- function(localMatrixX) cacheM <<- localMatrixX
+        
+        #create getter function to get value of cacheM from cache and return cacheM to the caller so we can check it for NULL
+        get_cacheM <- function() cacheM                       
+        list(set = setfunction, get = getfunction,
+             set_cacheM = set_cacheM,
+             get_cacheM = get_cacheM)
 }
 
+## This function computes the inverse of a matrix. 
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        #get value of localMatrixY from cache
+        localMatrixY<- x$get_cacheM()
+        #return cached matrix if available
+        if(!is.null(localMatrixY)) {
+                message("getting cached data")
+                return(localMatrixY)
+        }  
+        #if not returned yet, then matrix is not in cache
+        
+        #get original matrix
+        startM <- x$get()             
+        
+        #invert original matrix
+        endM <- solve(startM)
+        
+        #set inverted matrix in cache
+        x$set_cacheM(endM)
+        
+        #return inverted matrix
+        endM 
 }
